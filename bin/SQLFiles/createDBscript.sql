0aa4c65b08ee64cc89f07bcd908b91681d1e54bf
Anders Liltorp <anders.liltorp@steelseries.com>
2014-10-27 16:13:23 +0100

    FIX

--- bin/SQLFiles/createDBscript.sql
+++ bin/SQLFiles/createDBscript.sql
@@ -4,9 +4,9 @@ SET SESSION FOREIGN_KEY_CHECKS=0;
 
 /* Create Tables */
 
-CREATE TABLE IF NOT EXISTS Calender
+CREATE TABLE IF NOT EXISTS Calendar
 (
-	CalenderID int NOT NULL AUTO_INCREMENT,
+	CalendarID int NOT NULL AUTO_INCREMENT,
 	Name varchar(255) NOT NULL,
 	Active tinyint,
 	CreatedBy varchar(255) NOT NULL,
@@ -14,177 +14,7 @@ CREATE TABLE IF NOT EXISTS Calender
 	-- 2 = private
 	PrivatePublic tinyint NOT NULL COMMENT '1 = public
 	2 = private',
-	PRIMARY KEY (CalenderID)
-);
-
-
-CREATE TABLE IF NOT EXISTS dailyupdate
-(
-	date datetime NOT NULL UNIQUE,
-	apparentTemperature double,
-	summary text,
-	qotd varchar(300) NOT NULL,
-	msg_type varchar (100) NOT NULL,
-	update_timestamp TIMESTAMP DEFAULT NOW() ON UPDATE NOW(),
-	PRIMARY KEY (date)
-);
-
-
-CREATE TABLE IF NOT EXISTS events
-(
-	eventid int NOT NULL AUTO_INCREMENT,
-	type int NOT NULL,
-	location int,
-	createdby int NOT NULL,
-	start datetime NOT NULL,
-	end datetime NOT NULL,
-	name varchar(0) NOT NULL,
-	text text NOT NULL,
-	-- Decides wether the event is an import-event or user created
-	-- 
-	customevent boolean COMMENT 'Decides wether the event is an import-event or user created
-',
-	CalenderID int NOT NULL,
-	PRIMARY KEY (eventid)
-);
-
-
-CREATE TABLE IF NOT EXISTS locationdata
-(
-	locationdataid int NOT NULL AUTO_INCREMENT,
-	longitude int NOT NULL,
-	latitude int UNIQUE,
-	PRIMARY KEY (locationdataid)
-);
-
-
-CREATE TABLE IF NOT EXISTS notes
-(
-	noteId int NOT NULL AUTO_INCREMENT,
-	eventId int NOT NULL,
-	createdBy varchar(255) NOT NULL,
-	text text,
-	dateTime datetime NOT NULL,
-	active bit,
-	PRIMARY KEY (noteid)
-);
-
-
-
-CREATE TABLE IF NOT EXISTS roles
-(
-	roleid int NOT NULL AUTO_INCREMENT,
-	userid int NOT NULL,
-	type varchar(200) NOT NULL,
-	PRIMARY KEY (roleid)
-);
-
-
-CREATE TABLE IF NOT EXISTS userevents
-(
-	userid int NOT NULL,
-	CalenderID int NOT NULL
-);
-
-
-CREATE TABLE IF NOT EXISTS users
-(
-	userid int NOT NULL AUTO_INCREMENT,
-	email varchar(40) NOT NULL,
-	active boolean,
-	created datetime NOT NULL DEFAULT NOW(),
-	password varchar(200) NOT NULL,
-	PRIMARY KEY (userid)
-);
-
-/* Create Dummy Account */
-
-INSERT INTO `cbscalendar`.`users`
-(`email`,
-`active`,
-`password`)
-VALUES
-("admin@admin.dk",
-true,
-"d6YSr320JnLXlp8YYxUcNQ==");
-
-
-
-/* Create Foreign Keys */
-
-ALTER TABLE events
-	ADD FOREIGN KEY (CalenderID)
-	REFERENCES Calender (CalenderID)
-	ON UPDATE RESTRICT
-;
-
-
-ALTER TABLE userevents
-	ADD FOREIGN KEY (CalenderID)
-	REFERENCES Calender (CalenderID)
-	ON UPDATE RESTRICT
-;
-
-
-ALTER TABLE notes
-	ADD FOREIGN KEY (eventid)
-	REFERENCES events (eventid)
-	ON UPDATE RESTRICT
-;
-
-
-ALTER TABLE events
-	ADD FOREIGN KEY (location)
-	REFERENCES locationdata (locationdataid)
-	ON UPDATE RESTRICT
-;
-
-
-ALTER TABLE events
-	ADD FOREIGN KEY (createdby)
-	REFERENCES users (userid)
-	ON UPDATE RESTRICT
-;
-
-
-ALTER TABLE roles
-	ADD FOREIGN KEY (userid)
-	REFERENCES users (userid)
-	ON UPDATE RESTRICT
-;
-
-
-ALTER TABLE userevents
-	ADD FOREIGN KEY (userid)
-	REFERENCES users (userid)
-	ON UPDATE RESTRICT
-;
-
-
-ALTER TABLE notes
-	ADD FOREIGN KEY (createdby)
-	REFERENCES users (userid)
-	ON UPDATE RESTRICT
-;
-
-
-CREATE DATABASE IF NOT EXISTS cbscalendar;
-use cbscalendar;
-SET SESSION FOREIGN_KEY_CHECKS=0;
-
-/* Create Tables */
-
-CREATE TABLE IF NOT EXISTS Calender
-(
-	CalenderID int NOT NULL AUTO_INCREMENT,
-	Name varchar(255) NOT NULL,
-	Active tinyint,
-	CreatedBy varchar(255) NOT NULL,
-	-- 1 = public
-	-- 2 = private
-	PrivatePublic tinyint NOT NULL COMMENT '1 = public
-	2 = private',
-	PRIMARY KEY (CalenderID)
+	PRIMARY KEY (CalendarID)
 );
 
 
@@ -213,7 +43,7 @@ CREATE TABLE IF NOT EXISTS events
 	-- 
 	customevent boolean COMMENT 'Decides wether the event is an import-event or user created
 ',
-	CalenderID int NOT NULL,
+	CalendarID int NOT NULL,
 	PRIMARY KEY (eventid)
 );
 
@@ -250,7 +80,7 @@ CREATE TABLE IF NOT EXISTS roles
 CREATE TABLE IF NOT EXISTS userevents
 (
 	userid int NOT NULL,
-	CalenderID int NOT NULL
+	CalendarID int NOT NULL
 );
 
 
@@ -269,15 +99,15 @@ CREATE TABLE IF NOT EXISTS users
 /* Create Foreign Keys */
 
 ALTER TABLE events
-	ADD FOREIGN KEY (CalenderID)
-	REFERENCES Calender (CalenderID)
+	ADD FOREIGN KEY (CalendarID)
+	REFERENCES Calendar (CalendarID)
 	ON UPDATE RESTRICT
 ;
 
 
 ALTER TABLE userevents
-	ADD FOREIGN KEY (CalenderID)
-	REFERENCES Calender (CalenderID)
+	ADD FOREIGN KEY (CalendarID)
+	REFERENCES Calendar (CalendarID)
 	ON UPDATE RESTRICT
 ;
 
@@ -316,9 +146,8 @@ ALTER TABLE userevents
 	ON UPDATE RESTRICT
 ;
 
-
 ALTER TABLE notes
-	ADD FOREIGN KEY (createdby)
+	ADD FOREIGN KEY (createdBy)
 	REFERENCES users (userid)
 	ON UPDATE RESTRICT
 ;
--- src/encryption.java
+++ src/encryption.java
@@ -1,21 +1,17 @@
 import javax.xml.bind.ParseConversionEvent;
 
-import config.Configurations;
-
-
 public class encryption {
 //	Decryption path
 	public String decrypt(byte[] b)
 	{
-		Configurations CF = new Configurations();
 //		Defines the decryption value of the byte
 		//The 4 lines below needs to work later on, but for now, it will be hardcode
 		//System.out.println(CF.getFfcryptkey());
-		System.out.println(CF.getFfcryptkey());
-		String crypKey = CF.getFfcryptkey();
-		System.out.println(crypKey);
-		double gladKo = Double.parseDouble(crypKey);
-		byte ff = (byte) gladKo;
+		//System.out.println(CF.getFfcryptkey());
+		//String crypKey = CF.getFfcryptkey();
+		//System.out.println(crypKey);
+		//double gladKo = Double.parseDouble(crypKey);
+		byte ff = (byte) 3.1470;
 //		Generates for loop containing decryption value
 		for(int i = 0 ; i<b.length ; i++)
 		{
